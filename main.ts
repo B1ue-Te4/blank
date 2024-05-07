d0e9e1d15dc0be0e8e9d4cef28f66e527fd3e89a
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2024-05-07 10:52:29 +0000

    comit

--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
-import * as readline from "readline";
+import readline from "readline";
+import keypress from "keypress.js";
 import { Generate2DArray } from "./function";
 import { Visualize2DArray} from "./function";
 import { Block_T } from "./block";
+
+const kp = new keypress.Listener()
+
+process.stdin.on('keypress', function (ch, key) {
+    console.log('got "keypress"', key);
+    if (key && key.ctrl && key.name == 'c') {
+      process.stdin.pause();
+    }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const block = new Block_T
+const block = new Block_T;
 
 while (true) {
 rl.question("command?", input => {
