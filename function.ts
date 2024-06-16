da786fe7b80fc1a4a16acb4e0bc6a7a384d8d736
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2024-06-16 13:28:59 +0000

    2228

--- function.ts
+++ function.ts
@@ -1,5 +1,5 @@
 export function GenerateEntity (m: number, n: number, val = ' '): string[][][] {
-    return Array.from(new Array(m), _ => new Array(n).fill(new Array(1).fill(val)));
+    return Array.from(new Array(m), _ => [...new Array(n)].map(_ => new Array(1).fill(val)));
 }
 
 export function VisualizeEntity (entity: string[][][]): string {
